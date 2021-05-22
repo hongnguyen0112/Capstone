d59a2e8e099c71f006b3f91aa973446a532ab267
DanCat6300 <duylong6300@yahoo.com.vn>
2021-05-22 16:59:24 +0700

    Add comments and adjust layout

--- actions/actions.py
+++ actions/actions.py
@@ -6,66 +6,117 @@ from schema import schema
 from graph_database import GraphDatabase
 
 def resolve_mention(tracker: Tracker) -> Text:
+    """
+    Resolves a mention of an entity, such as first, to the actual entity.
+    If multiple entities are listed during the conversation, the entities
+    are stored in the slot 'listed_items' as a list. We resolve the mention,
+    such as first, to the list index and retrieve the actual entity.
+    :param tracker: tracker
+    :return: name of the actually entity
+    """
     graph_database = GraphDatabase()
     mention = tracker.get_slot("mention")
     listed_items = tracker.get_slot("listed_items")
+
     if mention is not None and listed_items is not None:
         idx = int(graph_database.map("mention_mapping", mention))
+
         if type(idx) is int and idx < len(listed_items):
             return listed_items[idx]
 
 
 def get_object_type(tracker: Tracker) -> Text:
+    """
+    Get the object_type synonym mentioned by the user. As the user may speak of an
+    entity type in plural, we need to map the mentioned synonym to the
+    object_type used in the knowledge base.
+    :param tracker: tracker
+    :return: object_type (same type as used in the knowledge base)
+    """
     graph_database = GraphDatabase()
     object_type = tracker.get_slot("object_type")
+
     return graph_database.map("object_type_mapping", object_type)
 
 
 def get_attribute(tracker: Tracker) -> Text:
+    """
+    Get the attribute mentioned by the user. As the user may use a synonym for
+    an attribute, we need to map the mentioned attribute to the
+    attribute name used in the knowledge base.
+    :param tracker: tracker
+    :return: attribute (same type as used in the knowledge base)
+    """
     graph_database = GraphDatabase()
     attribute = tracker.get_slot("attribute")
+
     return graph_database.map("attribute_mapping", attribute)
 
 
 def get_entity_name(tracker: Tracker, object_type: Text):
+    """
+    Get the name of the entity the user referred to. Either the NER detected the
+    entity and stored its name in the corresponding slot or the user referred to
+    the entity by an ordinal number, such as first or last, or the user refers to
+    an entity by its attributes.
+    :param tracker: Tracker
+    :param object_type: the object_type
+    :return: the name of the actual entity (value of key attribute in the knowledge base)
+    """
     mention = tracker.get_slot("mention")
+
     if mention is not None:
         return resolve_mention(tracker)
+
     entity_name = tracker.get_slot(object_type)
+
     if entity_name:
         return entity_name
+
     listed_items = tracker.get_slot("listed_items")
     attributes = get_attributes_of_entity(object_type, tracker)
+
     if listed_items and attributes:
         for entity in listed_items:
             graph_database = GraphDatabase()
+
             for entity in listed_items:
                 key_attr = schema[object_type]["key"]
                 result = graph_database.validate_entity(
                     object_type, entity, key_attr, attributes
                 )
+
                 if result is not None:
                     return to_str(result, key_attr)
+
     return None
 
 
 def get_attributes_of_entity(object_type, tracker):
+    # check what attributes the NER found for entity type
     attributes = []
     if object_type in schema:
+
         for attr in schema[object_type]["attributes"]:
             attr_val = tracker.get_slot(attr.replace("-", "_"))
+
             if attr_val is not None:
                 attributes.append({"key": attr, "value": attr_val})
+
     return attributes
 
 
 def reset_attribute_slots(slots, object_type, tracker):
+    # check what attributes the NER found for entity type
     if object_type in schema:
+
         for attr in schema[object_type]["attributes"]:
             attr = attr.replace("-", "_")
             attr_val = tracker.get_slot(attr)
+
             if attr_val is not None:
                 slots.append(SlotSet(attr, None))
+
     return slots
 
 
@@ -73,20 +124,35 @@ def to_str(
         entity: Dict[Text, Any],
         entity_keys: Union[Text, List[Text]]
     ) -> Text:
+    """
+    Converts an entity to a string by concatenating the values of the provided
+    entity keys.
+    :param entity: the entity with all its attributes
+    :param entity_keys: the name of the key attributes
+    :return: a string that represents the entity
+    """
     if isinstance(entity_keys, str):
         entity_keys = [entity_keys]
+
     v_list = []
     for key in entity_keys:
         _e = entity
         print("Got following entities:")
+
         for k in key.split("."):
             print(_e)
             _e = _e[k]
+
         v_list.append(str(_e))
+
     return ", ".join(v_list)
 
 
 class ActionQueryEntities(Action):
+    """
+    Action for listing entities.
+    The entities might be filtered by specific attributes.
+    """
 
     def name(self):
         return "action_query_entities"
@@ -94,22 +160,31 @@ class ActionQueryEntities(Action):
     def run(self, dispatcher, tracker, domain):
         graph_database = GraphDatabase()
 
+        # need to know the object_type we are looking for
         object_type = get_object_type(tracker)
         
+        # utter rephrase if found no object_type recognised
         if object_type is None:
             dispatcher.utter_template("utter_rephrase", tracker)
             return []
-            
+
+        # check what attributes the NER found for entity type
         attributes = get_attributes_of_entity(object_type, tracker)
         
+        # query knowledge base
         entities = graph_database.get_entities(object_type, attributes)
         
+        # utter message if no instance is found with the object_type
         if not entities:
             dispatcher.utter_template(
-                "I could not find any entities for '{}'.".format(object_type), tracker
+                "I could not find any entities for {}.".format(object_type), tracker
             )
             return []
         
+        """
+        Utter response and list all found instances 
+        """
+        # use the 'representation' attributes to print an entity
         entity_representation = schema[object_type]["representation"]
 
         dispatcher.utter_message(
@@ -125,6 +200,7 @@ class ActionQueryEntities(Action):
         # entities later on
         entity_key = schema[object_type]["key"]
         
+        #Number items in result list
         slots = [
             SlotSet("object_type", object_type),
             SlotSet("listed_items", list(map(lambda x: to_str(x, entity_key), entities))),
@@ -140,6 +216,9 @@ class ActionQueryEntities(Action):
 
     
 class ActionQueryAttribute(Action):
+    """
+    Action for querying a specific attribute of an entity.
+    """
 
     def name(self):
         return "action_query_attribute"
@@ -147,26 +226,32 @@ class ActionQueryAttribute(Action):
     def run(self, dispatcher, tracker, domain):
         graph_database = GraphDatabase()
 
+        # get entity type of entity
         object_type = get_object_type(tracker)
         
+        # utter rephrase if no object_type found
         if object_type is None:
             dispatcher.utter_template("utter_rephrase", tracker)
             return []
         
+        # get name of entity and attribute of interest
         name = get_object_type(tracker, object_type)
         attribute = get_attribute(tracker)
         
-        if name is None and attribute is None:
+        # utter rephrase if no name or attribute recognised
+        if name is None or attribute is None:
             dispatcher.utter_template("utter_rephrase", tracker)
             slots = [SlotSet("mention", None)]
             reset_attribute_slots(slots, object_type, tracker)
             return slots
 
+        # query knowledge base
         key_attribute = schema[object_type]["key"]
         value = graph_database.get_attribute_of(
             object_type, key_attribute, name, attribute 
         )
 
+        # utter response
         if value is not None and len(value) == 1:
             dispatcher.utter_message (
                 f"{name} has the value '{value[0]}' for attribute '{attribute}'."
@@ -181,9 +266,12 @@ class ActionQueryAttribute(Action):
         return slots
         
 
-
 #Action for resolving a mention
 class ActionResolveEntity(Action):
+    """
+    Action for resolving mention.
+    """
+
     def name (self):
         return "action_resolve_entity"
     
@@ -191,13 +279,16 @@ class ActionResolveEntity(Action):
         object_type = tracker.get_slot("object_type")
         listed_items = tracker.get_slot("listed_items")
 
+        # utter rephrase if no object_type found
         if object_type is None:
             dispatcher.utter_template("utter_rephrase", tracker)
             return []
         
+        # check if the entity is mentioned
         mention = tracker.get_slot("mention")
         if mention is not None:
             value = resolve_mention(tracker)
+            
             if value is not None: 
                 return [SlotSet(object_type, value), SlotSet("mention", None)]
         
--- graph_database.py
+++ graph_database.py
@@ -1,7 +1,6 @@
 import logging
 from typing import List, Dict, Any, Optional, Text
 from grakn.client import Grakn, SessionType, TransactionType
-from schema import schema
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +11,7 @@ class KnowledgeBase(object):
         self,
         object_type: Text,
         attributes: Optional[List[Dict[Text, Text]]] = None,
-        limit: int = 5,
+        limit: int = 20,
     ) -> List[Dict[Text, Any]]:
         raise NotImplementedError("Method is not implemented!")
 
@@ -35,6 +34,10 @@ class KnowledgeBase(object):
     
 
 class GraphDatabase(KnowledgeBase):
+    """
+    GraphDatabase uses a grakn graph database to encode domain knowledege. Make
+    sure to have the graph database set up and the grakn server running.
+    """
 
     def __init__ (
         self, 
@@ -45,30 +48,34 @@ class GraphDatabase(KnowledgeBase):
         self.keyspace = keyspace
 
     def _thing_to_dict(self, thing, transaction):
+        """
+        Converts a thing (a grakn object) to a dict for easy retrieval of the thing's
+        attributes.
+        """
         entity = {"id": thing.get_iid(), "type": thing.get_type().get_label().name()}
+
         for each in thing.as_remote(transaction).get_has():
             entity[each.get_type().get_label().name()] = each.get_value()
+
         return entity
 
     def _execute_entity_query(self, query: Text) -> List[Dict[Text, Any]]:
+        """
+        Executes a query that returns a list of entities with all their attributes.
+        """
         with Grakn.core_client(self.uri) as client:
             with client.session(self.keyspace, SessionType.DATA) as session:
                 with session.transaction(TransactionType.READ) as tx:
+
                     logger.debug("Executing Graql query: " + query)
                     result_iter = tx.query().match(query)
                     answers = [ans.get("product") for ans in result_iter]
                     entities = []
+
                     for c in answers:
                         entities.append(self._thing_to_dict(c, tx))
-                    return entities
 
-    # def _execute_attribute_query(self, query: Text) -> List[Any]:
-    #     with Grakn.core_client(self.uri) as client:
-    #         with client.session(self.keyspace, SessionType.DATA) as session:
-    #             with session.transaction(TransactionType.READ) as tx:
-    #                 print("Executing Graql Query: " + query)
-    #                 result_iter = tx.query().match(query)
-    #                 return [c.value() for c in result_iter]
+                    return entities
 
     def _execute_attribute_query(self, query: Text) -> List[Any]:
         """
@@ -78,11 +85,13 @@ class GraphDatabase(KnowledgeBase):
         with Grakn.core_client(self.uri) as client:
             with client.session(self.keyspace, SessionType.DATA) as session:
                 with session.transaction(TransactionType.READ) as tx:
+
                     print("Executing Graql Query: " + query)
                     query = "".join(query)
                     iterator = tx.query().match(query)   
                     answers = [ans.get('v') for ans in iterator]
                     result = [answer.get_value() for answer in answers]
+
                     return result
 
     def _execute_relation_query(
@@ -90,35 +99,48 @@ class GraphDatabase(KnowledgeBase):
         query: Text,
         relation_name: Text
     ) -> List[Dict[Text, Any]]:
+        """
+        Execute a query that queries for a relation. All attributes of the relation and
+        all entities participating in the relation are part of the result.
+        """
         with Grakn.core_client(self.uri) as client:
             with client.session(self.keyspace, SessionType.DATA) as session:
                 with session.transaction(TransactionType.READ) as tx:
+
                     print("Executing Graql Query: " + query)
                     result_iter = tx.query().match(query)
                     relations = []
+
                     for concept in result_iter:
                         relation_entity = concept.map().get(relation_name)
                         relation = self._thing_to_dict(relation_entity)
+
                         for (role_entity, entity_set) in relation_entity.role_players_map().items():
                             role_label = role_entity.label()
                             thing = entity_set.pop()
                             relation[role_label] = self._thing_to_dict(thing)
                         relations.append(relation)
+
                     return relations
 
-    # Construct attribute clause
     def _get_attribute_clause (
         self,
         attributes: Optional[List[Dict[Text, Text]]] = None
     ) -> Text:
+        """
+        Construct the attribute clause.
+        :param attributes: attributes
+        :return: attribute clause as string
+        """
         clause = ""
+
         if attributes:
             clause = ",".join([f"has {a['key']} '{a['value']}'" for a in attributes])
             clause = ", " + clause
             #, has key 'value', 
+
         return clause
     
-    # Get the value of the given attribute for the provided entity.
     def _get_attribute_of (
         self,
         object_type: Text,
@@ -126,6 +148,14 @@ class GraphDatabase(KnowledgeBase):
         entity: Text,
         attributes: Text
     ) -> List[Any]:
+        """
+        Get the value of the given attribute for the provided entity.
+        :param entity_type: entity type
+        :param key_attribute: key attribute of entity
+        :param entity: name of the entity
+        :param attribute: attribute of interest
+        :return: the value of the attribute
+        """
         return self._execute_attribute_query (
             f"""
                 match
@@ -139,9 +169,17 @@ class GraphDatabase(KnowledgeBase):
     def _get_product_entities (
         self,
         attributes: Optional[List[Dict[Text, Text]]] = None,
-        limit: int = 10
+        limit: int = 20
     ) -> List[Dict[Text, Any]]:
+        """
+        Query the graph database for product. Restrict the product
+        by the provided attributes, if any attributes are given.
+        :param attributes: list of attributes
+        :param limit: maximum number of products to return
+        :return: list of products
+        """
         attributes_clause = self._get_attribute_clause(attributes)
+
         return self._execute_entity_query (
             f"match "
             f"$product isa product{attributes_clause}; "
@@ -152,11 +190,21 @@ class GraphDatabase(KnowledgeBase):
         self, 
         object_type: Text,
         attributes: Optional[List[Dict[Text, Text]]] = None,
-        limit: int = 10
+        limit: int = 20
     ) -> List[Dict[Text, Any]]:
+        """
+        Query the graph database for entities of the given type. Restrict the entities
+        by the provided attributes, if any attributes are given.
+        :param entity_type: the entity type
+        :param attributes: list of attributes
+        :param limit: maximum number of entities to return
+        :return: list of entities
+        """
         if object_type == "product":
             return self._get_product_entities(attributes, limit)
+
         attribute_clause = self._get_attribute_clause(attributes)
+
         return self._execute_entity_query(
             f"match "
             f"${object_type} isa {object_type}{attribute_clause};"
@@ -168,6 +216,12 @@ class GraphDatabase(KnowledgeBase):
         mapping_type: Text,
         mapping_key: Text
     ) -> Text:
+        """
+        Query the given mapping table for the provided key.
+        :param mapping_type: the name of the mapping table
+        :param mapping_key: the mapping key
+        :return: the mapping value
+        """
         value = self._execute_attribute_query(
             f"match "
             f"$mapping isa {mapping_type}, "
@@ -175,20 +229,32 @@ class GraphDatabase(KnowledgeBase):
             f"has mapping_value $v;"
             f"get $v;"
         )
+
         print("mapping_value: ", value[0])
+
         if value and len(value) == 1:
             return value[0]
     
     def validate_entity(
         self, object_type, entity, key_attribute, attributes
     ) -> Dict[Text, Any]:
+        """
+        Validates if the given entity has all provided attribute values.
+        :param entity_type: entity type
+        :param entity: name of the entity
+        :param key_attribute: key attribute of entity
+        :param attributes: attributes
+        :return: the found entity
+        """
         attribute_clause = self._get_attribute_clause(attributes)
+
         value = self._execute_entity_query(
             f"match "
             f"${object_type} isa {object_type}{attribute_clause}, "
             f"has {key_attribute} '{entity}'; "
             f"get ${object_type};"
         )
+        
         if value and len(value) == 1:
             return value[0]
     
