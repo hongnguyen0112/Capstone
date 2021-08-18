4ada09f2b04f90f0b0679108c691257b5c37d393
s3694543 <s3694543@rmit.edu.vn>
2021-08-18 14:06:46 +0300

    Added script for typedb-console container

---
+++ typedb/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Abort on any error (including if wait-for-it fails).
+set -e
+
+# Wait for the backend to be up, if we know where it is.
+if [ -n "$HOST" ]; then
+   ./wait-for-it.sh "$HOST:${PORT:-1729}" 
+fi
+
+# Run the main container command.
+exec "$@"
