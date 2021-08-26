079ff2eaaebe87d4c57f0090a1354f9f34d8ea3f
s3694543 <s3694543@rmit.edu.vn>
2021-08-26 13:24:33 +0300

    Update home_screen

--- ui/lib/home_screen.dart
+++ ui/lib/home_screen.dart
@@ -14,8 +14,10 @@ class _HomeScreenState extends State<HomeScreen> {
   List<String> _data = [];
   static const String BOT_URL = "http://localhost:5005/webhooks/rest/webhook";
   TextEditingController queryController = TextEditingController();
+  List fallback_values = [];
+  bool fallback = false;
   Map<String, String> requestHeaders = {
-    "Content-type" : "application/json",
+    "Content-type": "application/json",
   };
   ScrollController _scrollController = new ScrollController();
 
@@ -36,60 +38,51 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Expanded(
-            child:Container( 
-           padding: EdgeInsets.all(10),
-            child: AnimatedList(
-              physics: BouncingScrollPhysics(),
-              controller: _scrollController,
-              key: _listKey,
-              initialItemCount: _data.length,
-              itemBuilder: (BuildContext context, int index,
-                  Animation<double> animation) {
-                return buildItem(_data[index], animation, index);
-              }
-              )
-              )),
-        Row(
-           children: [
-       Expanded(
-         child: Container(
-           padding: EdgeInsets.all(10),
-           child: TextField(
-              style: TextStyle(color: Colors.black),
-              decoration: InputDecoration(
-                filled: true,
-                hintText: "Say Hi!",
-                fillColor: Colors.grey[100],
-                border:OutlineInputBorder(
-                  borderSide: BorderSide(width:0),
-                  gapPadding: 10,
-                  borderRadius:BorderRadius.circular(25) )
-              ),
-              
-              controller: queryController,
-              textInputAction: TextInputAction.send,
-              onSubmitted: (msg) {
-                this.getResponse();
-              },
-            )
-            )
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: AnimatedList(
+                      physics: BouncingScrollPhysics(),
+                      controller: _scrollController,
+                      key: _listKey,
+                      initialItemCount: _data.length,
+                      itemBuilder: (BuildContext context, int index,
+                          Animation<double> animation) {
+                        return buildItem(_data[index], animation, index);
+                      }))),
+          Row(children: [
+            Expanded(
+                child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                          filled: true,
+                          hintText: "Say Hi!",
+                          fillColor: Colors.grey[100],
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 0),
+                              gapPadding: 10,
+                              borderRadius: BorderRadius.circular(25))),
+                      controller: queryController,
+                      textInputAction: TextInputAction.send,
+                      onSubmitted: (msg) {
+                        this.getResponse();
+                      },
+                    ))),
+            SizedBox(
+              width: 5,
             ),
-           SizedBox(width: 5,),
-           Padding(padding: EdgeInsets.only(right: 10),
-           child:GestureDetector(
-          onTap: () => {this.getResponse()},
-           child: Container(
-             padding: EdgeInsets.all(7),
-             decoration: BoxDecoration(
-               shape: BoxShape.circle,
-               color: Colors.blue
-             ),
-             child: Icon(Icons.send,color: Colors.white),
-           )
-           ) )
-           ]
-             ),
-          
+            Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: GestureDetector(
+                    onTap: () => {this.getResponse()},
+                    child: Container(
+                      padding: EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.blue),
+                      child: Icon(Icons.send, color: Colors.white),
+                    )))
+          ]),
         ],
       ),
     );
@@ -101,34 +94,69 @@ class _HomeScreenState extends State<HomeScreen> {
     if (queryController.text.length > 0) {
       this.insertSingleItem(queryController.text);
       var client = getClient();
+    
       try {
         client.post(Uri.parse(BOT_URL),
             headers: requestHeaders,
-            body:
-                jsonEncode({"sender": 'test', 'message': queryController.text}))
+            body: jsonEncode({
+              "sender": 'test',
+              'message': fallback
+                  ? fallback_values[
+                      int.parse(queryController.text.toString()) - 1]
+                  : queryController.text
+            }))
           ..then((response) {
             List<dynamic> data = jsonDecode(response.body);
-            Map messageData = data[0];
+            Map? messageData = data[0];
+            List? buttons = messageData?['buttons'];
             // if the size of the list is less then 1 then insert only one item
-            if (data.length <= 1) {
+            if (data.length <= 1 &&
+                messageData != null &&
+                !messageData.keys.contains('buttons')) {
               insertSingleItem(messageData['text'] + '<bot>');
+            } else if (messageData != null &&
+                messageData.keys.contains('buttons') &&
+                buttons != null) {
+              String message = '';
+              for (int i = 0; i < buttons.length; i++) {
+                int count = i + 1;
+                message +=
+                    ("\n" + count.toString() + "." + " " + buttons[i]['title']);
+                //Saving the button payload into a list
+                setState(() {
+                  fallback_values.add(buttons[i]['title']);
+                  fallback = true;
+                });
+              }
+              insertSingleItem(messageData['text'] +
+                  message +
+                  '\n' +
+                  '\n' +
+                  '[Please Enter the corresponding number]' +
+                  '<bot>');
             }
             // else insert all the items
-            else {
+            else if (data.length >= 2 && messageData != null ) {
               String message = '';
               data.forEach((e) => {message += (e['text'] + "\n")});
               insertSingleItem(message + '<bot>');
+            } else if(data.isEmpty){
+              insertSingleItem("Sorry, this feature is under maintaneces..." + '<bot>');
             }
           });
       } catch (e) {
         print(e);
+          insertSingleItem("Sorry, this feature is under maintaneces..." + '<bot>');
         return null;
       } finally {
         client.close();
         queryController.clear();
+        this.setState(() {
+          fallback = false;
+        });
       }
-      _scrollController.animateTo(
-    _scrollController.position.maxScrollExtent, curve: Curves.easeInOut,duration: Duration(milliseconds: 200));
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+          curve: Curves.easeInOut, duration: Duration(milliseconds: 200));
     }
   }
 
@@ -164,3 +192,5 @@ Widget buildItem(String item, Animation<double> animation, int index) {
     ),
   );
 }
+
+
